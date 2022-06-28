4f07a1c7c3a446e279aa342cd05902664b4a0f3b
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-28 23:49:52 +0300

    0.1 Finished F1

--- ex5.py
+++ ex5.py
@@ -12,11 +12,21 @@ def names_of_registered_students(input_json_path, course_name):
     :param course_name: The name of the course.
     :return: List of the names of the students.
     """
+
+    with open(input_json_path, 'r') as input_file:
+        loaded_students = json.load(input_file)
+
+    required_students = []
+
+    for student_info in loaded_students.values():
+        tmp_courses = student_info["registered_courses"]
+        for course in tmp_courses:
+            if course == course_name:
+                student_name = student_info["student_name"]
+                required_students.append(student_name)
+    return required_students
     pass
 
-"""
-hello
-"""
 def enrollment_numbers(input_json_path, output_file_path):
     """
     This function writes all the course names and the number of enrolled
@@ -26,8 +36,6 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param output_file_path: Path of the output text file.
     """
     pass
-
-
 
 def courses_for_lecturers(json_directory_path, output_json_path):
     """
